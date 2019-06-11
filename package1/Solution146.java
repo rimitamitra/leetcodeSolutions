f5dd45dbb86963acba6a8da72c87b45c5fdbba35
Rimita-Daikin <rimita.mitra@dsv.daikin.com>
2019-06-10 17:13:57 -0700

    Prob 146
    Prob 146

---
+++ package1/Solution146.java
@@ -0,0 +1,39 @@
+/**
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+The cache is initialized with a positive capacity.
+*/
+
+
+
+
+import java.util.*; 
+class LRUCache {
+    
+    LinkedHashMap<Integer, Integer> map;
+    public LRUCache(int capacity) {
+        
+        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true){
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest){
+                return size() > capacity;
+            }
+        };
+    }
+    
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+    
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
